4ac0a4410b5274f3d7e1e9945d60849ac127d93c
Sebastian Christensen <sebc8@hotmail.com>
2023-05-10 11:07:20 +0200

    contact page

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,40 +1,22 @@
-import { Link } from "react-router-dom";
-import forside from "../img/forside.png";
-import forsideWeb from "../img/forside-web.png";
-import logo from "../img/logo.png";
-
 export default function Kontakt() {
   return (
-    <div className="min-h-screen">
+    <div>
+      <h1>Nysgerrig på noget?</h1>
+      <p>
+        Her i NSSA arbejder vi med unge atleter, trænere, colleges og klubber
+        fra hele verden i forskellige tidszoner. Har du spørgsmål om det vi gør,
+        vores klienter eller blot vil vide mere om college og hvordan vi måske
+        kan hjælpe dig? Ring eller skriv til os på email og vi vil gøre alt vi
+        kan for at være behjælpelige.{" "}
+      </p>
       <div>
-        <img
-          className="object-cover bg-left bg-no-repeat h-full w-full absolute -z-10 lg:hidden"
-          src={forside}
-          alt=""
-        />
-        <img
-          className="lg:object-cover lg:bg-center lg:bg-no-repeat lg:h-full lg:w-full lg:absolute  lg:-z-10 hidden lg:block"
-          src={forsideWeb}
-          alt=""
-        />
+        <h3>+45 26 19 93 28</h3>
       </div>
-
-      <div className="min-h-screen flex flex-col lg:justify-between items-center">
-        <div>
-          <img className="mx-auto pt-10 px-10" src={logo} alt="logo"></img>
-        </div>
-        <div className="lg:pb-40 my-auto lg:my-0 space-x-10">
-          <Link to="/login">
-            <button className="border rounded-lg bg-[#1FB9F1] text-white px-10 py-3 lg:px-12 lg:py-4 ">
-              Login
-            </button>
-          </Link>
-          <Link to="/new-user">
-            <button className="border rounded-lg bg-white text-[#021965] px-10 py-3 lg:px-12 lg:py-4">
-              Opret
-            </button>
-          </Link>
-        </div>
+      <div>
+        <h3>Hello@nssa.dk</h3>
+      </div>
+      <div>
+        <h3>Willemoesgade 12 2100, København Ø</h3>
       </div>
     </div>
   );
