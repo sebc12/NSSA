f998980f2da6dccf4b3d5a7c8ee92bc807137a52
AsadSi <asadullah@live.dk>
2023-05-11 12:34:09 +0200

    tarik

--- src/pages/Matchmaking.jsx
+++ src/pages/Matchmaking.jsx
@@ -1,8 +1,22 @@
 import logo from "../img/logo.png";
+import forside from "../img/forside.png";
+import forsideWeb from "../img/forside-web.png";
 
 export default function Matchmaking() {
   return (
-    <div className="min-h-screen flex flex-col justify-between bg-slate-500 px-10">
+    <div className="min-h-screen flex flex-col justify-between px-10">
+            <div>
+        <img
+          className="object-cover bg-left bg-no-repeat h-full w-full absolute -z-10 lg:hidden"
+          src={forside}
+          alt=""
+        />
+        <img
+          className="lg:object-cover lg:bg-center lg:bg-no-repeat lg:h-full lg:w-full lg:absolute  lg:-z-10 hidden lg:block"
+          src={forsideWeb}
+          alt=""
+        />
+      </div>
       <div>
         <img className="mx-auto pt-5 h-16" src={logo} alt="logo"></img>
         <h1 className="text-center text-4xl my-10 text-white">
@@ -47,16 +61,39 @@ export default function Matchmaking() {
 
           <select name="" id="">
             <option value="">Budget</option>
+            <option value="">0-5000 USD</option>
+            <option value="">6-8000 USD</option>
+            <option value="">9-11000 USD</option>
+            <option value="">12-14000 USD</option>
+            <option value="">15-18000 USD</option>
+            <option value="">19k+ USD</option>
           </select>
           <select name="" id="">
-            <option value="">Akademisk</option>
+            <option value="">10.5 - 12</option>
+            <option value="">8.5 - 10.4</option>
+            <option value="">7 - 8.4</option>
+            <option value="">4.5 - 6.9</option>
+            <option value="">2.1 - 4.4</option>
+            <option value="">0 - 2.0</option>
           </select>
 
           <select name="" id="">
             <option value="">Athletism</option>
+            <option value="">Meget atletisk</option>
+            <option value="">Over middel</option>
+            <option value="">middel</option>
+            <option value="">Under middel</option>
+            <option value="">Ikke atletisk</option>
+            <option value="">Intet</option>
           </select>
           <select name="" id="">
             <option value="">Hold niveau</option>
+            <option value="">Superliga / 1 Division</option>
+            <option value="">2 Division / Liga Top</option>
+            <option value="">3 Division / Liga Midt / Div Top</option>
+            <option value="">DS / Liga Bund / Div Midt</option>
+            <option value="">KS / SS / JS / Serie 1 / Liga 1 / Øst 1</option>
+            <option value="">Ingen betydende CV</option>
           </select>
         </form>
       </div>
