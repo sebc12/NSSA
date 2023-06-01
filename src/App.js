45ad41e86f50b808c0d6337100864aad707559d1
Sebastian Christensen <sebc8@hotmail.com>
2023-06-01 13:11:48 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,23 +8,19 @@ import Kontakt from "./pages/Kontakt";
 import Matchmaking from "./pages/Matchmaking";
 import Indstillinger from "./pages/Indstillinger";
 import Matches from "./pages/Matches";
-import { useState } from "react";
+
 import PrivateRoute from "../src/componment/PrivateRoute";
 
 function App() {
-  const [token, setToken] = useState("");
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<NSSA />} />
-            <Route
-              path="Signin"
-              element={<Signin token={token} setToken={setToken} />}
-            />
+            <Route path="Signin" element={<Signin />} />
             <Route path="signup" element={<Signup />} />
-            <Route element={<PrivateRoute token={token} />}>
+            <Route element={<PrivateRoute />}>
               <Route path="menu" element={<Menu />} />
               <Route path="kontakt" element={<Kontakt />} />
               <Route path="matchmaking" element={<Matchmaking />} />
